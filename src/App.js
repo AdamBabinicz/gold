e9d660502144c1db509429f9268bfc0b0ff691ae
babinicz <babinicz@onet.pl>
2022-01-25 14:01:22 +0100

    rejestracja w google

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import Contact from "./components/Contact";
 import ScrollToTop from "./ScrollToTop";
 import Scroll from "./Scroll";
+import CookieConsent from "react-cookie-consent";
 // import Footer from "./components/Footer";
 
 function App() {
@@ -16,6 +17,33 @@ function App() {
         <Route path="/kontakt" component={Contact} exact></Route>
         <Redirect to="/"></Redirect>
       </Switch>
+      <CookieConsent
+        debug={true}
+        location="bottom"
+        style={{
+          background: "#333",
+          textAlign: "left",
+          paddingBottom: "1rem",
+          fontSize: "16px",
+          fontFamily: "Gideon Roman",
+        }}
+        buttonStyle={{
+          color: "#333",
+          background: "#fff",
+          fontSize: "18px",
+          fontFamily: "Gideon Roman",
+          marginRight: "1rem",
+        }}
+        buttonText="OK, rozumiem"
+        expires={365}
+      >
+        "W ramach naszej witryny stosujemy pliki cookies w celu świadczenia
+        Państwu usług na najwyższym poziomie, w tym w sposób dostosowany do
+        indywidualnych potrzeb. Korzystanie z witryny bez zmiany ustawień
+        dotyczących cookies oznacza, że będą one zamieszczane w Państwa
+        urządzeniu końcowym. Możecie Państwo dokonać w każdym czasie zmiany
+        ustawień dotyczących cookies."
+      </CookieConsent>
       {/* <Footer /> */}
     </>
   );
